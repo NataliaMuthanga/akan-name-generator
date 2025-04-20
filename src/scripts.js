6f117325e47dee3e96aaa39aa8d119dd6f713983
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-20 18:48:40 +0300

    edit html, javascript and css files

--- src/scripts.js
+++ src/scripts.js
@@ -1,27 +1,31 @@
-let heading1 = document.getElementById("hd1")
-heading1.addEventListener("mouseover", function(){
-    heading1.style.color = "red"
-})
-heading1.addEventListener("mouseleave", function(){
-    heading1.style.color = "black"
-})
+document.getElementById("akanfm").addEventListener("submit", function(e){
+    e.preventDefault();
 
-const maleNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame"
-];
-const femaleNames = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama"
-];
+    const dateInput = document.getElementById("birthday").value;
+    const genderInput = document.querySelector("input[name='gender']:checked");
 
+    if (!dateInput || !genderInput) {
+        alert("Fill in all fields.");
+        return;
+    }
+
+    const gender = genderInput.value;
+    const date = new Date(dateInput);
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    if (day < 1 || day > 31 || month < 1 || month > 12) {
+        alert("Enter valid date.");
+        return;
+    }
+
+    const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+    const dayOfWeek = date.getDay();
+    const akanName = gender === "male" ? maleNames[dayOfWeek] : femaleNames[dayOfWeek]
+
+    document.getElementById("result").textContent = 
+    "Your Akan Name is ${akanName}"
+})
