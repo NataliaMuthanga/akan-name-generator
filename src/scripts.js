7e947ba0387fdb34071a59915fd50c3029c929cf
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-20 12:36:47 +0300

    add syles to heading1

---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+let heading1 = document.getElementById("hd1")
+heading1.addEventListener("mouseover", function(){
+    heading1.style.color = "red"
+})
+heading1.addEventListener("mouseleave", function(){
+    heading1.style.color = "black"
+})
+
